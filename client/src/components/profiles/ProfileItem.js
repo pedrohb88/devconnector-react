547338e20064c771164e4f80ed1dc1c5fee44237
Pedro Leal <pedrohb88@hotmail.com>
2020-07-26 13:11:25 -0300

    language fix 2.0

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -14,7 +14,7 @@ const ProfileItem = ({ profile: {
             <img src={avatar} alt="" className="round-img" />
             <div>
                 <h2>{name}</h2>
-                <p>{status} {company && <span> at {company}</span>}</p>
+                <p>{status} {company && <span> na(o) {company}</span>}</p>
                 <p className="my-1">{location && <span>{location}</span>}</p>
                 <Link to={`/profile/${_id}`} className="btn btn-primary" >Ver Perfil</Link>
             </div>
