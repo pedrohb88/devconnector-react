279ddc504c09c74cea3b38fe1b998e2e61cc9371
Pedro Leal <pedrohb88@hotmail.com>
2020-08-04 20:57:02 -0300

    test build commit

--- test/api/routes/posts.test.js
+++ test/api/routes/posts.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../../../server');
 
 describe('GET /posts', () => {
-
+    //temp update to test build
     it('should return 401 status for no authorization token request on private route', (done) => {
         request(app)
             .get('/api/posts')
