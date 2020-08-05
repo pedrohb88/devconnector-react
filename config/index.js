f513feaf6f857521cf2a6decdb0aaceeeb958f74
Pedro Leal <pedrohb88@hotmail.com>
2020-08-04 21:22:03 -0300

    test

--- config/index.js
+++ config/index.js
@@ -1,5 +1,7 @@
 let env = process.env.NODE_ENV || 'development';
 
+console.log('ENV: ', env);
+
 if(env === 'development' || env === 'test'){
     let config = require('./config.json'); //automatically parses json
     let envConfig = config[env];
