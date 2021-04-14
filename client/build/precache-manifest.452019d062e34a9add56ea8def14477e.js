f619c6d23a6cc88ce158329e4d53754418bd1e6d
Pedro Leal <pedrohb88@hotmail.com>
2021-04-14 20:49:26 -0300

    remote backup

---
+++ client/build/precache-manifest.452019d062e34a9add56ea8def14477e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adaa24080c469a57c4aa95e5d4f8623d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3e5beac43384618e424",
+    "url": "/static/css/main.86b541e6.chunk.css"
+  },
+  {
+    "revision": "661b5a1c9b7fbdb7f02e",
+    "url": "/static/js/2.f866aa71.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.f866aa71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3e5beac43384618e424",
+    "url": "/static/js/main.df1e7d99.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "d66c8dc37259d97af295becf8d4c2ba2",
+    "url": "/static/media/blankAvatar.d66c8dc3.jpg"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
